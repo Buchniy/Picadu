237aba6ed7603feba476383d490b3fb87e450eea
Buch <hhccuubb@ukr.net>
2020-11-12 21:28:31 +0200

    day3

--- js/main.js
+++ js/main.js
@@ -23,13 +23,16 @@ const editPhotoURL = document.querySelector('.edit-photo');
 const  userAvatarElem = document.querySelector('.user-avatar');
 
 const postWrapper = document.querySelector('.posts');
+const buttonNewPost = document.querySelector('.button-new-post');
+const addPostElem = document.querySelector('.add-post');
 
 
 const listUsers = [
   {
     email: 'buch@gmail.com',
     password: '123',
-    displayName: 'Buch'
+    displayName: 'Buch',
+    photo:'https://i.pinimg.com/originals/70/44/f8/7044f887031b89cb1025f20c7a0005f9.jpg'
   },
   {
     email: 'foo@gmail.com',
@@ -101,7 +104,7 @@ const  setPosts = {
       title: 'Заголовлок поста',
       text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet blanditiis consequatur corporis cum deleniti dicta, dolore enim, esse eum exercitationem expedita fuga inventore itaque minus nostrum nulla odit perferendis, placeat praesentium quam repellendus reprehenderit veniam voluptatem voluptatibus. Beatae debitis deleniti eaque earum eos eum fuga nemo optio repellendus temporibus. Ab alias blanditiis deserunt ducimus ea fugit impedit laborum minima molestiae molestias necessitatibus nemo optio quia, quos recusandae rerum velit voluptatem. Alias autem blanditiis consectetur dolore doloremque doloribus eveniet exercitationem, nostrum officiis quam quis repudiandae suscipit velit. Asperiores earum iste, libero minus nam provident reprehenderit rerum sapiente sed similique voluptate!',
       tags: ['свежее','новое','горячее','мое','случайность'],
-      author: 'buch@gmail.com',
+      author: {displayName: 'maks', photo:'https://i.pinimg.com/originals/70/44/f8/7044f887031b89cb1025f20c7a0005f9.jpg'},
       date: '11.11.2020, 20:54:00',
       like: 45,
       comments: 20
@@ -110,7 +113,7 @@ const  setPosts = {
       title: 'Заголовлок поста 2',
       text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его',
       tags: ['свежее','новое','горячее','мое','случайность'],
-      author: 'foo@gmail.com',
+      author: {displayName: 'anna', photo: 'https://cs9.pikabu.ru/post_img/2020/02/10/6/1581327942162716565.jpg'},
       date: '11.11.2020, 20:54:00',
       like: 30,
       comments: 12
@@ -119,12 +122,30 @@ const  setPosts = {
       title: 'Заголовлок поста 3',
       text: 'Далеко-далеко вершину  продолжил парадигматическая? Но языком сих пустился, запятой своего его',
       tags: ['свежее','новое','горячее','мое','случайность'],
-      author: 'booooo@gmail.com',
+      author: {displayName: 'ganna', photo: 'https://cs.pikabu.ru/post_img/2013/10/23/8/1382531440_1694009757.jpg'},
       date: '11.11.2020, 20:54:00',
       like: 393,
       comments: 5
     }
-  ]
+  ],
+  addPost(title, text, tags, handler){
+    this.allPosts.unshift({
+      title,
+      text,
+      tags: tags.split(',').map(item => item.trim()),
+      author: {
+        displayName: setUsers.user.displayName,
+        photo: setUsers.user.photo
+      },
+      date: new Date().toLocaleString(),
+      like: 0,
+      comments: 0
+    });
+
+    if(handler){
+      handler();
+    }
+  }
 };
 
 const toggleAuthDom = () => {
@@ -135,19 +156,29 @@ const toggleAuthDom = () => {
     userElem.style.display = "";
     userNameElem.textContent = user.displayName;
     userAvatarElem.src = user.photo || userAvatarElem.src;
+    buttonNewPost.classList.add('visible');
   }else{
     loginElem.style.display = "";
     userElem.style.display = "none";
+    buttonNewPost.classList.remove('visible');
+    addPostElem.classList.remove('visible');
+    postWrapper.classList.add('visible');
+
+
   }
 };
 
+const showAddPost = () => {
+  addPostElem.classList.add('visible');
+  postWrapper.classList.remove('visible');
+};
 
 const showAllPosts = () => {
 
   let postsHTML = '';
-  setPosts.allPosts.forEach(({ title, text, date, author, tags }) => {
+  setPosts.allPosts.forEach(({ title, text, date, tags, like, author, comments }) => {
 
-    const displayName = author.split('@', 1).join();
+
 
     postsHTML += `
     <section class="post">
@@ -155,7 +186,8 @@ const showAllPosts = () => {
            <h2 class="post-title">${title}</h2>
            <p class="post-text">${text}</p>
            <div class="tags">
-             <a href="#" class="tag">${tags}#свежее</a>
+           ${tags.map(tag => `<a href="#" class="tag">#${tag}</a>`)}
+             
            </div>
          </div>
          <div class="post-footer">
@@ -164,13 +196,13 @@ const showAllPosts = () => {
                <svg width="19" height="20" class="icon icon-like">
                  <use xlink:href="img/icons.svg#like"></use>
                </svg>
-               <span class="likes-counter">26</span>
+               <span class="likes-counter">${like}</span>
              </button>
              <button class="post-button comments">
                <svg width="21" height="21" class="icon icon-comment">
                  <use xlink:href="img/icons.svg#comment"></use>
                </svg>
-               <span class="comments-counter">157</span>
+               <span class="comments-counter">${comments}</span>
              </button>
              <button class="post-button save">
                <svg width="19" height="19" class="icon icon-save">
@@ -186,10 +218,10 @@ const showAllPosts = () => {
            <!-- /.post-buttons -->
            <div class="post-author">
              <div class="author-about">
-               <a href="#" class="author-username">${displayName}</a>
+               <a href="#" class="author-username">${author.displayName}</a>
                <span class="post-time">${date}</span>
              </div>
-             <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+             <a href="#" class="author-link"><img src=${author.photo} || "img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
            </div>
            <!-- /.post-author -->
          </div>
@@ -197,8 +229,14 @@ const showAllPosts = () => {
        </section>
       `;
   });
+
   postWrapper.innerHTML = postsHTML;
+
+  addPostElem.classList.remove('visible');
+  postWrapper.classList.add('visible');
 };
+
+
 
 const init = () => {
   loginForm.addEventListener('submit', (event) => {
@@ -240,9 +278,33 @@ const init = () => {
     // вешаем класс на меню, когда кликнули по кнопке меню
     menu.classList.toggle('visible');
   });
+
+  buttonNewPost.addEventListener('click', event => {
+    event.preventDefault();
+    showAddPost();
+  });
+
+  addPostElem.addEventListener('submit', event => {
+    event.preventDefault();
+    const { title, text, tags } = addPostElem.elements;
+    if(title.value.length < 6){
+      alert('короткий заголовок');
+      return;
+    }
+    if(text.value.length < 50){
+      alert('короткий пост');
+      return;
+    }
+
+    setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
+    addPostElem.classList.remove('visible');
+    addPostElem.reset();
+  });
+
   showAllPosts();
   toggleAuthDom();
 };
 
 document.addEventListener('DOMContentLoaded', init);
 
+//buch@gmail.com
